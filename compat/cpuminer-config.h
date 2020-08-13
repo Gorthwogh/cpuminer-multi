f1d664dec15d45f0ee5c64fa1f8a81d30c6cbf18
Gorthwogh <51260391+Gorthwogh@users.noreply.github.com>
2020-08-13 14:15:29 +0200

    update version

--- compat/cpuminer-config.h
+++ compat/cpuminer-config.h
@@ -85,25 +85,25 @@
 /* #undef NO_MINUS_C_MINUS_O */
 
 /* Name of package */
-#define PACKAGE "cpuminer-multi"
+#define PACKAGE "cpuminer-multi-aarch64"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "cpuminer-multi"
+#define PACKAGE_NAME "cpuminer-multi-aarch64"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "cpuminer-multi 1.3.7"
+#define PACKAGE_STRING "cpuminer-multi-aarch64 1.3.8"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "cpuminer-multi"
+#define PACKAGE_TARNAME "cpuminer-multi-aarch64"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.3.7"
+#define PACKAGE_VERSION "1.3.8"
 
 /* If using the C implementation of alloca, define if you know the
    direction of stack growth for your system; otherwise it will be
@@ -132,7 +132,7 @@
 #define USE_XOP 1
 
 /* Version number of package */
-#define VERSION "1.3.7"
+#define VERSION "1.3.8"
 
 /* Define to `unsigned int' if <sys/types.h> does not define. */
 /* #undef size_t */
